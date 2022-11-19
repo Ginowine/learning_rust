d19d78b5853828ca7296c92fd52a3e027e2ffb4c
Gino <ginowine1@gmail.com>
2022-11-19 00:06:23 -0800

    add rust slice example

---
+++ slice_type_example/src/main.rs
@@ -0,0 +1,22 @@
+fn main() {
+
+    let mut s = String::from("Hello world");
+
+    let word = first_word(&s);
+
+    println!("{}", word);
+
+    s.clear();
+    
+}
+
+fn first_word(s: &String) -> usize {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return i;
+        }
+    }
+    s.len()
+}
