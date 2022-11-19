6d65bb8aacc132173decd81318767854e7eb1826
Gino <ginowine1@gmail.com>
2022-11-19 00:42:26 -0800

    update slice example

--- slice_type_example/src/main.rs
+++ slice_type_example/src/main.rs
@@ -10,13 +10,13 @@ fn main() {
     
 }
 
-fn first_word(s: &String) -> usize {
+fn first_word(s: &String) -> &str {
     let bytes = s.as_bytes();
 
     for (i, &item) in bytes.iter().enumerate() {
         if item == b' ' {
-            return i;
+            return &s[0..i];
         }
     }
-    s.len()
+    &s[..]
 }
