13f5e75e9012eb2151714d00026dae69c885e881
Gino <gino.osahon@gmail.com>
2022-11-23 05:43:14 -0800

    using slice on array

--- slice_type_example/src/main.rs
+++ slice_type_example/src/main.rs
@@ -6,11 +6,44 @@ fn main() {
 
     println!("The first word is: {}", word);
 
+    let my_string = String::from("hello world");
+
+    let word = first_word(&my_string[0..6]);
+
+    println!("The first word is: {}", word);
+
+    let my_string = String::from("hello world");
+
+    let word_one = first_word(&my_string[..]);
+
+    println!("The first word is: {}", word_one);
+
+    let my_string_literal = "hello world";
+
+    let word = first_word(&my_string_literal[0..6]);
+
+    println!("The first word is: {}", word);
+
+    let word = first_word(&my_string_literal[..]);
+
+    println!("The first word is: {}", word);
+
+    let word = first_word(my_string_literal);
+
+    println!("The first word is: {}", word);
+
     s.clear();
+
+    let a = [1, 2, 3, 4, 5];
+
+    let slice = &a[1..3];
+
+    assert_eq!(slice, &[2, 3]);
+
     
 }
 
-fn first_word(s: &String) -> &str {
+fn first_word(s: &str) -> &str {
     let bytes = s.as_bytes();
 
     for (i, &item) in bytes.iter().enumerate() {
