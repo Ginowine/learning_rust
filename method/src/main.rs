40e4799b8ebf87c40e6242eab6c5318c9a9404d4
Gino <gino.osahon@gmail.com>
2022-11-24 12:35:02 -0800

    add method example

---
+++ method/src/main.rs
@@ -0,0 +1,19 @@
+fn main() {
+    let rect1 = Rectangle{
+        width: 30,
+        height: 50,
+    };
+
+    println!("The area of the rectangle is {} square pixels.", rect1.area());
+}
+
+struct Rectangle{
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle{
+    fn area(&self) -> u32{
+        self.width * self.height
+    }
+}
