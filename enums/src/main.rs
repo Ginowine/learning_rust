f2fd76fad6f77aee49c1c4d92c685662dfd03013
Gino <gino.osahon@gmail.com>
2022-12-31 03:54:29 -0800

    modify enums example

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -1,25 +1,13 @@
 fn main() {
     #[derive(Debug)]
-    enum IpAddrKind {
-        v4,
-        v6,
+    enum IpAddr {
+        V4(String),
+        V6(String),
     }
 
-    #[derive(Debug)]
-    struct IpAddr {
-        kind: IpAddrKind,
-        address: String,
-    }
+    let home = IpAddr::V4(String::from("127.0.0.1"));
 
-    let home = IpAddr{
-        kind: IpAddrKind::v4,
-        address: String::from("127.0.0.1"),
-    };
-
-    let loopback = IpAddr{
-        kind: IpAddrKind::v6,
-        address: String::from("..1"),
-    };
+    let loopback = IpAddr::V6(String::from("::1"));
 
     println!("{:?}", home);
 }
