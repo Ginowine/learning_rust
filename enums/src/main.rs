f398ead0874aa274c75b98aed81b2aa94a7672da
Gino <gino.osahon@gmail.com>
2022-12-31 04:15:50 -0800

    update enums

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -11,3 +11,19 @@ fn main() {
 
     println!("{:?}", home);
 }
+// defining method for an enum as you would do in struct 
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
+
+impl Message {
+    fn call (&self) {
+        // method body would be defined here
+    }
+}
+
+let m = Message::Write(String::from("hello"));
+m.call();
