eb2d15138affbe950338189a799907ab10cc7279
Gino <gino.osahon@gmail.com>
2023-03-13 12:31:20 -0700

    initialising a string

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -1,19 +1,13 @@
 mod front_of_house {
-    mod hosting {
-        fn add_to_waitlist(){}
-
-        fn seat_at_table() {}
+    pub mod hosting {
+        pub fn add_to_waitlist(){}
     }
+}
 
-    mod serving {
-        fn take_order() {
-            
-        }
-        fn serve_order() {
-            
-        }
-        fn take_payment() {
-            
-        }
-    }
+pub fn  eat_at_restaurant() {
+    // Absolute path
+    crate::front_of_house::hosting::add_to_waitlist();
+
+    // Relative path
+    front_of_house::hosting::add_to_waitlist();
 }
---
+++ string_example/src/main.rs
@@ -0,0 +1,7 @@
+fn main() {
+    let data = "initial content";
+    let s = data.to_string();
+
+    let s = "initial content".to_string();
+    println!("{s}");
+}
