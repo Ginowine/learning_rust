ad75d1630039fc17a8e2d0b1bf30f3cf5a6413e5
Gino <gino.osahon@gmail.com>
2023-03-14 07:52:20 -0700

    iterating over HashMaps

--- hash_maps/src/main.rs
+++ hash_maps/src/main.rs
@@ -15,4 +15,11 @@ fn main() {
     for (key, value) in scores {
         println!("{key}: {value}");
     }
+
+    let field_name = String::from("Favorite color");
+    let field_value = String::from("Blue");
+
+    let mut map = HashMap::new();
+    map.insert(field_name, field_value);
+
 }
