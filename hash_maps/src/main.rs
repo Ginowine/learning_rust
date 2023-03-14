5c0bc8d09b74b6a9cd0e8fb96cd11ec8c851d9e1
Gino <gino.osahon@gmail.com>
2023-03-14 07:32:15 -0700

    iterating over HashMaps

--- hash_maps/src/main.rs
+++ hash_maps/src/main.rs
@@ -5,4 +5,14 @@ fn main() {
 
     scores.insert(String::from("Blue"), 10);
     scores.insert(String::from("Yellow"), 50);
+
+    let team_name = String::from("Blue");
+
+    let score = scores.get(&team_name).copied().unwrap_or(0);
+
+    println!("{score}");
+
+    for (key, value) in scores {
+        println!("{key}: {value}");
+    }
 }
