392c24da20518b6baf0ceb92f27c991dcf40bdb4
Gino <ginowine1@gmail.com>
2023-03-18 09:45:51 -0700

    converting a string to pig latin

---
+++ strings_to_pig_latin/target/debug/deps/strings_to_pig_latin-81027814f0917a45.d
@@ -0,0 +1,5 @@
+/Users/gino/rust_projects/strings_to_pig_latin/target/debug/deps/strings_to_pig_latin-81027814f0917a45: src/main.rs
+
+/Users/gino/rust_projects/strings_to_pig_latin/target/debug/deps/strings_to_pig_latin-81027814f0917a45.d: src/main.rs
+
+src/main.rs:
---
+++ strings_to_pig_latin/target/debug/strings_to_pig_latin.d
@@ -0,0 +1,1 @@
+/Users/gino/rust_projects/strings_to_pig_latin/target/debug/strings_to_pig_latin: /Users/gino/rust_projects/strings_to_pig_latin/src/main.rs
