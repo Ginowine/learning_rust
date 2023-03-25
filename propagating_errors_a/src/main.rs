4d11d11ff699c515c3875fe1143ee4b22c1654eb
Gino <ginowine1@gmail.com>
2023-03-25 05:17:22 -0700

    propagate errors using the? operator

--- propagating_errors_a/src/main.rs
+++ propagating_errors_a/src/main.rs
@@ -11,6 +11,10 @@ fn main() {
         // This a shorter form of opening and reading a file
         // File::open("hello.txt")?.read_to_string(&mut username)?;
 
+        // The standard library provides a shorter function that opens the file, creates a new String, 
+        // reads the contents of the file, puts the contents into that String, and returns it.
+        // fs::read_to_string("hello.txt")
+
         Ok(username)
     }
 }
