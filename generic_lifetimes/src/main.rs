c5fdba5136bdb11fbd0685b31e6a7a6b2cf21dd3
Gino <ginowine1@gmail.com>
2023-04-05 00:51:55 -0700

    update longest function

--- generic_lifetimes/src/main.rs
+++ generic_lifetimes/src/main.rs
@@ -8,4 +8,10 @@ fn main() {
     println!("The longest string is: {}", result);
 }
 
-fn longest()
+fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+}
