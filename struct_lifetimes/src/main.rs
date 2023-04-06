663fe038d92549b5ba456e2921d2a441c3b18047
Gino <ginowine1@gmail.com>
2023-04-05 23:19:03 -0700

    implement lifetimes in struct

---
+++ struct_lifetimes/src/main.rs
@@ -0,0 +1,17 @@
+#[derive(Debug)]
+struct ImportantExcerpt<'a>{
+    part: &'a str,
+}
+
+
+fn main() {
+
+    let novel = String::from("Call me Ishmael. Some years ago...");
+    let first_sentence = novel.split('.').next().expect("Could not find a '.'");
+
+    let i = ImportantExcerpt{
+        part: first_sentence,
+    };
+
+    println!("The first sentence is: {:?}", i);
+}
