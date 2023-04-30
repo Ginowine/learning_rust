eb6f4338bd9ae3fe7fa8505ee4eb8c00cd169fe3
Gino <ginowine1@gmail.com>
2023-04-30 09:47:05 -0700

    return an option type frp, a function

---
+++ option-type/src/main.rs
@@ -0,0 +1,18 @@
+#![allow(unused)]
+fn main() {
+fn divide(numerator: f64, denominator: f64) -> Option<f64> {
+    if denominator == 0.0 {
+        None
+    } else {
+        Some(numerator / denominator)
+    }
+}
+
+let result = divide(2.0, 3.0);
+
+match result {
+    Some(x) => println!("Result: {x}"),
+
+    None    => println!("Cannot divide by 0"),
+}
+}
