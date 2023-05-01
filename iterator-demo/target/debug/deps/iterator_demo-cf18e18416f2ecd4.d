4ac28fad398419e6917a54619f835f877b57b0a5
Gino <ginowine1@gmail.com>
2023-05-01 16:29:34 +0100

    iterate on vector

---
+++ iterator-demo/target/debug/deps/iterator_demo-cf18e18416f2ecd4.d
@@ -0,0 +1,7 @@
+/Users/gino/rust_projects/iterator-demo/target/debug/deps/iterator_demo-cf18e18416f2ecd4.rmeta: src/lib.rs
+
+/Users/gino/rust_projects/iterator-demo/target/debug/deps/libiterator_demo-cf18e18416f2ecd4.rlib: src/lib.rs
+
+/Users/gino/rust_projects/iterator-demo/target/debug/deps/iterator_demo-cf18e18416f2ecd4.d: src/lib.rs
+
+src/lib.rs:
---
+++ iterator-demo/target/debug/deps/iterator_demo-ec00353d3c86ba9e.d
@@ -0,0 +1,5 @@
+/Users/gino/rust_projects/iterator-demo/target/debug/deps/iterator_demo-ec00353d3c86ba9e: src/lib.rs
+
+/Users/gino/rust_projects/iterator-demo/target/debug/deps/iterator_demo-ec00353d3c86ba9e.d: src/lib.rs
+
+src/lib.rs:
