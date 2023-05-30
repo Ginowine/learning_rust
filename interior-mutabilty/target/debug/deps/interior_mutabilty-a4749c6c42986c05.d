c453a0a00217e0fc91dc0c020e92871822140670
Gino <ginowine1@gmail.com>
2023-05-30 19:41:25 +0100

    use mock objects in rust

---
+++ interior-mutabilty/target/debug/deps/interior_mutabilty-a4749c6c42986c05.d
@@ -0,0 +1,7 @@
+/Users/gino/rust_projects/interior-mutabilty/target/debug/deps/interior_mutabilty-a4749c6c42986c05.rmeta: src/lib.rs
+
+/Users/gino/rust_projects/interior-mutabilty/target/debug/deps/libinterior_mutabilty-a4749c6c42986c05.rlib: src/lib.rs
+
+/Users/gino/rust_projects/interior-mutabilty/target/debug/deps/interior_mutabilty-a4749c6c42986c05.d: src/lib.rs
+
+src/lib.rs:
---
+++ interior-mutabilty/target/debug/libinterior_mutabilty.d
@@ -0,0 +1,1 @@
+/Users/gino/rust_projects/interior-mutabilty/target/debug/libinterior_mutabilty.rlib: /Users/gino/rust_projects/interior-mutabilty/src/lib.rs
