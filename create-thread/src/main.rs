31773b98952c816ac2274ecc751f8729ef416761
Gino <ginowine1@gmail.com>
2023-05-31 13:42:04 +0100

    block main thread until spwaned thread finishes

--- create-thread/src/main.rs
+++ create-thread/src/main.rs
@@ -3,7 +3,7 @@ use  std::time::Duration;
 
 fn main() {
 
-    thread::spawn(|| {
+    let handle = thread::spawn(|| {
         for i in 1..10 {
             println!("hi number {} from the spawned thread!", i);
             thread::sleep(Duration::from_millis(1));
@@ -14,4 +14,6 @@ fn main() {
         println!("hi number {} from the main thread!", i);
         thread::sleep(Duration::from_millis(1));
     }
+
+    handle.join().unwrap();
 }
