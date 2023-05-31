2c6ec3ca2808387f42596de4813d2123367b3c9a
Gino <ginowine1@gmail.com>
2023-05-31 05:17:48 +0100

    implement interior mutability

--- interior-mutabilty/src/lib.rs
+++ interior-mutabilty/src/lib.rs
@@ -38,22 +38,23 @@ where
     #[cfg(test)]
     mod tests {
         use super::*;
+        use std::cell::RefCell;
 
         struct MockMessenger {
-            sent_messages: Vec<String>,
+            sent_messages: RefCell<Vec<String>>,
         }
 
         impl MockMessenger {
             fn new() -> MockMessenger {
                 MockMessenger {
-                    sent_messages: vec![],
+                    sent_messages: RefCell::new(vec![]);
                 }
             }
         }
 
         impl Messenger for MockMessenger {
             fn send (&self, message: &str){
-                self.sent_messages.push(String::from(message));
+                self.sent_messages.borrow_mut().push(String::from(message));
             }
         }
         #[test]
@@ -63,6 +64,6 @@ where
 
             limit_tracker.set_value(80);
 
-            assert_eq!(mock_messanger.sent_messages.len(), 1);
+            assert_eq!(mock_messanger.sent_messages.borrow().len(), 1);
         }
     }
